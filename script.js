35dc90246fb637d94e7ab214bc8ae23e51d621f8
bertrandsimon <trandber@hotmail.com>
2023-01-24 15:41:09 +0100

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('script loaded')
+
+document.addEventListener("DOMContentLoaded", function () {
+    const elem = document.querySelector('input[name="foo"]');
+    const datepicker = new Datepicker(elem, {
+      // ...options
+    });
+});
